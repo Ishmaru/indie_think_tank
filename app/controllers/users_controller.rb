7dca67cff34972722c755d6d88ffb9e7165b41c3
Adrian Delpha <adrian.delpha@gmail.com>
2016-06-05 17:12:42 -0700

    basic user implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to show_user_path
+    else
+      render 'edit'
+    end
+  end
+
+
  def destroy
     @user = User.find(params[:id])
     @user.destroy
